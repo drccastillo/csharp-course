d70f879adbae835484b37b7bd593f628a2815169
darwin-dev <ccdarwinpmp@gmail.com>
2025-06-09 16:02:44 -0400

    style: extract magic values to constants and improve coding standards in DependencyInversion module

--- DependencyInversion/WeatherApp/Services/HttpWeatherProvider.cs
+++ DependencyInversion/WeatherApp/Services/HttpWeatherProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http.Json;
 using WeatherApp.Interfaces;
 using WeatherApp.Models;
@@ -8,11 +9,14 @@ public class HttpWeatherProvider(HttpClient httpClient) : IWeatherProvider
 {
   private readonly HttpClient _httpClient = httpClient;
 
+  private const string ForecastUrlTemplate =
+    "https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
   // TODO: Create a level of http abstraction for http requests into HttpService with provided url
   public async Task<double> GetTodayAsync(string latitude, string longitude)
   {
-    var response = await _httpClient.GetFromJsonAsync<ForecastDto>(
-      $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
+    var url = string.Format(ForecastUrlTemplate, latitude, longitude);
+    var response = await _httpClient.GetFromJsonAsync<ForecastDto>(url);
 
     // TODO: Apply validators or error handling for response
     return response!.current.temperature_2m;
