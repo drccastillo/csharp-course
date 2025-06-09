366505543750eca2cda8c1f4ddb758cf17b84efc
darwin-dev <ccdarwinpmp@gmail.com>
2025-06-09 16:02:44 -0400

    style: extract magic values to constants and improve coding standards in DependencyInversion module

--- DependencyInversion/WeatherApp/Cli/WeatherCli.cs
+++ DependencyInversion/WeatherApp/Cli/WeatherCli.cs
@@ -1,3 +1,4 @@
+using System;
 using WeatherApp.Interfaces;
 
 namespace WeatherApp.Cli;
@@ -6,11 +7,16 @@ public class WeatherCli(IWeatherProvider provider)
 {
   private readonly IWeatherProvider _provider = provider;
 
+  private const string DefaultLatitude = "-16.54649";
+  private const string DefaultLongitude = "-68.058805";
+
   public async Task RunAsync(string[] args)
   {
     // TODO: Create validator for the args
     // OPTIONAL: Separate the arguments
-    var coordinates = (args.Length > 0 ? args[0] : "-16.54649,-68.058805").Split(',');
+    var coordinates = args.Length > 0
+      ? args[0].Split(',', StringSplitOptions.RemoveEmptyEntries)
+      : new[] { DefaultLatitude, DefaultLongitude };
     // TODO: Validate that coordinates are valid coordinates
     var latitude = coordinates[0];
     var longitude = coordinates[1];
--- DependencyInversion/WeatherApp/Services/SecondHttpWeatherProvider.cs
+++ DependencyInversion/WeatherApp/Services/SecondHttpWeatherProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http.Json;
 using WeatherApp.Interfaces;
 using WeatherApp.Models;
@@ -8,11 +9,14 @@ public class SecondHttpWeatherProvider(HttpClient httpClient) : IWeatherProvider
 {
   private readonly HttpClient _httpClient = httpClient;
 
+  private const string ForecastUrlTemplate =
+    "https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
   public async Task<double> GetTodayAsync(string latitude, string longitude)
   {
     // OPTIONAL: Search for a new endpoint to get the weather (refactor scaffolding for models)
-    var response = await _httpClient.GetFromJsonAsync<ForecastDto>(
-      $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m");
+    var url = string.Format(ForecastUrlTemplate, latitude, longitude);
+    var response = await _httpClient.GetFromJsonAsync<ForecastDto>(url);
 
     return response!.current.temperature_2m;
   }
